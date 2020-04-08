28050121a3c8a2bd26d9832d4a99e5a779304a15
inuse918 <s2018s34@e-mirim.hs.kr>
2020-04-09 02:53:23 +0900

    clock

--- chromeApp/index.js
+++ chromeApp/index.js
@@ -1,25 +1,18 @@
 const TITLE=document.querySelector("#title")   
 
-const BASE_COLOR="rgb(0, 0, 0)";
-const OTHER_COLOR="rgb(500,0,0)"
-// function changeColor(){
-//     if(title.color=="red"){
-//         title.style.color="black"  ;
-//     }else if(title.color=="black"){
-//         title.style.color="red";
-//     }
-// }
+const CLICKED_CLASS = "clicked";
 
 function handleClick(){
-    const currentColor=title.style.color
-    if (currentColor===BASE_COLOR){
-        title.style.color=OTHER_COLOR;
-    }else {
-        title.style.color=BASE_COLOR;
+    const currentClass = title.className;
+    if(currentClass!==CLICKED_CLASS){
+        title.className=CLICKED_CLASS;
+    }else{
+        title.className="";
     }
 }
+
 function init(){
-    title.addEventListener("mouseenter",handleClick)
+    title.addEventListener("click ",handleClick)
 }
 init()
 
