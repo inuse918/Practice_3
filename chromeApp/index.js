ec9589d1cb8f24b14448cc6487d135fb68e2fb0c
inuse918 <s2018s34@e-mirim.hs.kr>
2020-04-09 00:56:08 +0900

    no message

--- chromeApp/index.js
+++ chromeApp/index.js
@@ -1,5 +1,25 @@
-const title=document.querySelector("#title")
-title.innerHTML="Hi! From JS"
-title.style.color="blue"
-explain.style.color="black"
-document.title="I own you now"
+const TITLE=document.querySelector("#title")   
+
+const BASE_COLOR="rgb(0, 0, 0)";
+const OTHER_COLOR="rgb(500,0,0)"
+// function changeColor(){
+//     if(title.color=="red"){
+//         title.style.color="black"  ;
+//     }else if(title.color=="black"){
+//         title.style.color="red";
+//     }
+// }
+
+function handleClick(){
+    const currentColor=title.style.color
+    if (currentColor===BASE_COLOR){
+        title.style.color=OTHER_COLOR;
+    }else {
+        title.style.color=BASE_COLOR;
+    }
+}
+function init(){
+    title.addEventListener("mouseenter",handleClick)
+}
+init()
+
